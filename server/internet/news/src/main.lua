1ff651f4faad81c47503706e0e8b47334f0891c5
Beard-Dev <suchyj126@gmail.com>
2025-12-13 17:02:56 +0000

    Edited News Logic

--- server/internet/news/src/main.lua
+++ server/internet/news/src/main.lua
@@ -1,4 +1,22 @@
-local news = GGHJK Has Official Internet - 13.12.2025
+function loadUI()
+term.clear()
+term.setBackgroundColor(colors.black)
+term.setTextColor(colors.white)
+term.clear()
+end
+local news = 'GGHJK Has Official Internet - 13.12.2025'
+loadUI()
+print("News:")
+print(news)
+print("")
+print('"exit" pro ukonceni')
+function wait()
+local rsp = read()
+if rsp == "exit" then
+return
+else
+wait()
+end
+end
 
-print ("News:")
-print (news)
+read()

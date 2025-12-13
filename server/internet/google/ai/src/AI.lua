933f645b13232924584228417032abb3e241f507
Beard-Dev <suchyj126@gmail.com>
2025-12-13 13:51:08 +0100

    Obscure API key in AI.lua

--- server/internet/google/ai/src/AI.lua
+++ server/internet/google/ai/src/AI.lua
@@ -1,4 +1,4 @@
-local GEMINI_API_KEY = "PUT Here Your Key"
+local GEMINI_API_KEY = "*****************"
 local GEMINI_ENDPOINT = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" .. GEMINI_API_KEY
 local MODEL = "gemini-2.5-flash"
 

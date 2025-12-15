965b82853a71d4a577d6912b483541081ead4e87
Beard-Dev <207234327+MrBeard-dev@users.noreply.github.com>
2025-12-15 17:50:00 +0000

    Webbroswer now working

--- client/src/web-client.lua
+++ client/src/web-client.lua
@@ -3,7 +3,7 @@ local TEMP_FILE = ".website.lua"
 local BACKCOLOR = colors.gray
 local TEXTCOLOR = colors.orange
 -------------------------------------------
-local ver = "1.6.1"
+local ver = 1.61
 local nverurl = 'https://raw.githubusercontent.com/MC-GGHJK/MC-Server-Internet/refs/heads/main/client/version/version.txt'
 
 local nver = "?"
@@ -24,7 +24,7 @@ local w, h = term.getSize()
     term.write("(c)2025 GGHJK - Internet browser 2025")
     term.setCursorPos(1,h-2)
     term.write('Version '..ver..' / '..nver)
-    if ver =< nver then
+    if ver < nver then
       term.setCursorPos(1,h-1)
       term.write('New version '..nver..' available!')
     end

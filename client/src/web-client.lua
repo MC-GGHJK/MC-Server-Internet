deaec6cc99cb6d93833f8c6504df2e9cf9f19ada
Beard-Dev <207234327+MrBeard-dev@users.noreply.github.com>
2025-12-16 17:08:41 +0000

    New Version Numbering

--- client/src/web-client.lua
+++ client/src/web-client.lua
@@ -3,7 +3,7 @@ local TEMP_FILE = ".website.lua"
 local BACKCOLOR = colors.gray
 local TEXTCOLOR = colors.orange
 -------------------------------------------
-local ver = 1.69
+local ver = "1.6.9"
 local nverurl = 'https://raw.githubusercontent.com/MC-GGHJK/MC-Server-Internet/refs/heads/main/client/version/version.txt'
 
 local nver = "?"

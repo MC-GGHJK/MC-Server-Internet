98537119f7dbc03fb3eeb0bba3a68a13fe5dfe94
Beard-Dev <207234327+MrBeard-dev@users.noreply.github.com>
2026-01-10 14:36:06 +0000

    Added old system for update checking

--- client/src/web-client.lua
+++ client/src/web-client.lua
@@ -15,18 +15,12 @@ local function drawUI()
     term.clear()
     
     term.setCursorPos(1, 1)
-    term.setBackgroundColor(colors.lightGray)
-    term.clearLine()
-    term.setTextColor(colors.gray)
-    term.write(" [ Novy tab ]  [ Domu ]  [ + ]")
-
-    term.setCursorPos(1, 2)
     term.setBackgroundColor(colors.white)
     term.clearLine()
     term.setTextColor(colors.gray)
     term.write(" (i)  gghjk://")
     
-    term.setCursorPos(1, 3)
+    term.setCursorPos(1, 2)
     term.setBackgroundColor(BACKCOLOR)
     term.setTextColor(colors.black)
     term.write(string.rep("-", w))
@@ -34,42 +28,38 @@ local function drawUI()
     term.setBackgroundColor(colors.black)
     term.setCursorPos(1, h)
     term.clearLine()
-    term.setTextColor(colors.lightGray)
-    term.write(" v" .. VER)
-
-    local ver = VER
-    local nver_num = tonumber(nver) or nver
-
-    if nver == "?" then
-        term.setCursorPos(1, h - 1)
-        term.write('Unable to check for updates!')
-    elseif type(nver_num) == "number" and ver < nver_num then
-        term.setCursorPos(1, h - 1)
-        term.write('A new version ' .. nver .. ' available!')
-    elseif type(nver_num) == "number" and ver >= nver_num then
-        term.setCursorPos(1, h - 1)
-        term.write('You are up to date!')
-    elseif nver == "?" then
-        term.setCursorPos(1, h - 1)
-        term.write('Unable to check for updates!')
-    elseif ver == nver then
-        term.setCursorPos(1, h - 1)
-        term.write('You are up to date!')
-    else
-        term.setCursorPos(1, h - 1)
-        term.write('Unable to check for updates!')
-    end
-
-    term.setBackgroundColor(BACKCOLOR)
-    term.setTextColor(colors.white)
-    term.setCursorPos(1, h - 2)
-    term.write("(c) 2025-2026 GGHJK Web Engine")
     
-    term.setCursorPos(15, 2)
-    term.setBackgroundColor(colors.white)
-    term.setTextColor(colors.black)
-end
+    if ver < nver then
 
+      term.setCursorPos(1,h-1)
+
+      term.write('A new version '..nver..' available!')
+
+    elseif ver >= nver then
+
+      term.setCursorPos(1,h-1)
+
+      term.write('You are up to date!')
+
+    elseif nver == "?" then
+
+      term.setCursorPos(1,h-1)
+
+      term.write('Unable to check for updates!')
+
+    elseif ver == nver then
+
+      term.setCursorPos(1,h-1)
+
+      term.write('You are up to date!')
+
+    else
+
+      term.setCursorPos(1,h-1)
+
+      term.write('Unable to check for updates!')
+
+    end
 local function checkVersion()
     local r = http.get(NVERURL)
     if r then
@@ -81,7 +71,7 @@ end
 local function showError(msg)
     term.setBackgroundColor(BACKCOLOR)
     term.setTextColor(colors.red)
-    term.setCursorPos(1, 5)
+    term.setCursorPos(1, 4)
     print(" [!] ERROR: " .. msg)
     sleep(3)
     drawUI()
@@ -89,7 +79,7 @@ end
 
 local function fetchAndRun(domain)
     term.setBackgroundColor(BACKCOLOR)
-    term.setCursorPos(1, 5)
+    term.setCursorPos(1, 4)
     term.setTextColor(colors.white)
     print(" Propojovani s dns://" .. domain .. "...")
 
@@ -147,7 +137,7 @@ checkVersion()
 drawUI()
 
 while true do
-    term.setCursorPos(15, 2)
+    term.setCursorPos(15, 1)
     term.setBackgroundColor(colors.white)
     term.setTextColor(colors.black)
     
@@ -156,7 +146,7 @@ while true do
     if domain_input and domain_input ~= "" and not tonumber(domain_input) then
         fetchAndRun(domain_input)
     else
-        term.setCursorPos(1, 5)
+        term.setCursorPos(1, 4)
         term.setBackgroundColor(BACKCOLOR)
         term.setTextColor(colors.red)
         print(" Neplatna URL adresa!")

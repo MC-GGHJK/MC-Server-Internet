dc5a31d24d0fbbb8e1e0838e34b4ad7c4c77115a
Beard-Dev <207234327+MrBeard-dev@users.noreply.github.com>
2026-01-10 14:33:50 +0000

    Added old system for update checking

--- client/src/web-client.lua
+++ client/src/web-client.lua
@@ -2,7 +2,7 @@ local SERVER_ID = 133
 local TEMP_FILE = ".website.lua"
 local BACKCOLOR = colors.gray
 local TEXTCOLOR = colors.orange
-local VER = 2.0
+local VER = 1.9
 local NVERURL = 'https://raw.githubusercontent.com/MC-GGHJK/MC-Server-Internet/refs/heads/main/client/version/version.txt'
 
 local nver = "?"
@@ -15,12 +15,18 @@ local function drawUI()
     term.clear()
     
     term.setCursorPos(1, 1)
+    term.setBackgroundColor(colors.lightGray)
+    term.clearLine()
+    term.setTextColor(colors.gray)
+    term.write(" [ Novy tab ]  [ Domu ]  [ + ]")
+
+    term.setCursorPos(1, 2)
     term.setBackgroundColor(colors.white)
     term.clearLine()
     term.setTextColor(colors.gray)
     term.write(" (i)  gghjk://")
     
-    term.setCursorPos(1, 2)
+    term.setCursorPos(1, 3)
     term.setBackgroundColor(BACKCOLOR)
     term.setTextColor(colors.black)
     term.write(string.rep("-", w))
@@ -28,34 +34,38 @@ local function drawUI()
     term.setBackgroundColor(colors.black)
     term.setCursorPos(1, h)
     term.clearLine()
-    
     term.setTextColor(colors.lightGray)
     term.write(" v" .. VER)
-    
-    local statusMsg = "System OK"
-    local statusColor = colors.green
-    
+
+    local ver = VER
+    local nver_num = tonumber(nver) or nver
+
     if nver == "?" then
-        statusMsg = "Offline Check"
-        statusColor = colors.yellow
-    elseif VER < tonumber(nver or 0) then
-        statusMsg = "Update available (" .. nver .. ")"
-        statusColor = colors.red
+        term.setCursorPos(1, h - 1)
+        term.write('Unable to check for updates!')
+    elseif type(nver_num) == "number" and ver < nver_num then
+        term.setCursorPos(1, h - 1)
+        term.write('A new version ' .. nver .. ' available!')
+    elseif type(nver_num) == "number" and ver >= nver_num then
+        term.setCursorPos(1, h - 1)
+        term.write('You are up to date!')
+    elseif nver == "?" then
+        term.setCursorPos(1, h - 1)
+        term.write('Unable to check for updates!')
+    elseif ver == nver then
+        term.setCursorPos(1, h - 1)
+        term.write('You are up to date!')
+    else
+        term.setCursorPos(1, h - 1)
+        term.write('Unable to check for updates!')
     end
-    
-    term.setCursorPos(w - #statusMsg, h)
-    term.setTextColor(statusColor)
-    term.write(statusMsg)
 
     term.setBackgroundColor(BACKCOLOR)
     term.setTextColor(colors.white)
     term.setCursorPos(1, h - 2)
     term.write("(c) 2025-2026 GGHJK Web Engine")
-    term.setCursorPos(1, h - 1)
-    term.setTextColor(colors.lightGray)
-    term.write("Discord: #gghjk-internet")
     
-    term.setCursorPos(15, 1)
+    term.setCursorPos(15, 2)
     term.setBackgroundColor(colors.white)
     term.setTextColor(colors.black)
 end
@@ -71,7 +81,7 @@ end
 local function showError(msg)
     term.setBackgroundColor(BACKCOLOR)
     term.setTextColor(colors.red)
-    term.setCursorPos(1, 4)
+    term.setCursorPos(1, 5)
     print(" [!] ERROR: " .. msg)
     sleep(3)
     drawUI()
@@ -79,7 +89,7 @@ end
 
 local function fetchAndRun(domain)
     term.setBackgroundColor(BACKCOLOR)
-    term.setCursorPos(1, 4)
+    term.setCursorPos(1, 5)
     term.setTextColor(colors.white)
     print(" Propojovani s dns://" .. domain .. "...")
 
@@ -137,7 +147,7 @@ checkVersion()
 drawUI()
 
 while true do
-    term.setCursorPos(15, 1)
+    term.setCursorPos(15, 2)
     term.setBackgroundColor(colors.white)
     term.setTextColor(colors.black)
     
@@ -146,7 +156,7 @@ while true do
     if domain_input and domain_input ~= "" and not tonumber(domain_input) then
         fetchAndRun(domain_input)
     else
-        term.setCursorPos(1, 4)
+        term.setCursorPos(1, 5)
         term.setBackgroundColor(BACKCOLOR)
         term.setTextColor(colors.red)
         print(" Neplatna URL adresa!")

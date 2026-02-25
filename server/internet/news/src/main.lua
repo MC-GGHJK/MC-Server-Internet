c19f257141f497a8201983ddae34b09ebe06c8fd
Beard-Dev <207234327+MrBeard-dev@users.noreply.github.com>
2026-02-25 19:49:52 +0000

    Update News in news.lua

--- server/internet/news/src/main.lua
+++ server/internet/news/src/main.lua
@@ -5,9 +5,14 @@ term.setTextColor(colors.white)
 term.clear()
 end
 local news = 'GGHJK Has Official Internet - 13.12.2025'
+local news2 = 'GGHJK Firmware 1 - 1.2.2026'
+local news3 = 'GGHJK Internet 2.0 - 6.2.2026'
+local news4 = 'GGHJK Internet 2.1 - 20.3.2026'
 loadUI()
 print("News:")
 print(news)
+print(news2)
+print(news3)
 print("")
 print('"exit" pro ukonceni')
 function wait()
